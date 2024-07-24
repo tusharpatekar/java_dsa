61b387c82b57716c75b46f80c62d74d977e0deb6
tusharpatekar <tusharpatekar45@gmail.com>
2024-07-25 00:15:09 +0530

    DP

---
+++ dynamicProgramming/fibnacchiExample.java
@@ -0,0 +1,19 @@
+package dynamicProgramming;
+
+public class fibnacchiExample {
+
+    public static int fib (int n, int[] f){
+        if(n==0 || n==1){
+            return n;
+        }
+        if(f[n] != 0){
+            return f[n];
+        }
+        f[n] = fib(n-1, f) + fib(n-2, f);
+        return f[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(5, new int[6]));
+    }
+}
